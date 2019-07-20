06c27d63fb96a44b2ac0c4629b77ad83c45a2145
RoSpr <rsprikut@mail.ru>
2019-07-20 20:24:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import argparse
 import os
 import requests
 
+load_dotenv()
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
 
 def is_bitlink(token, link):
     if link.startswith("http://"):
         link = link.replace("http://", "")
-    elif link.startswith("https://"):
+    if link.startswith("https://"):
         link = link.replace("https://", "")
 
     url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(link)
 
     headers = {
-        "Authorization": ACCESS_TOKEN
+        "Authorization": token
     }
 
     response = requests.get(url, headers=headers)
@@ -55,49 +58,44 @@ def count_clicks(token, link):
     return statistics
 
 
-if __name__ == "__main__":
-    load_dotenv()
-    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+def cut_http(link):
+    if link.startswith("http://"):
+        newlink = link.replace("http://", "")
+        return newlink
+    if link.startswith("https://"):
+        newlink = link.replace("https://", "")
+        return newlink
+    return link
 
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("link", help="creates a  bitlink if standard link is given (e.g. 'http://google.com');"
                                      "displays sum of link clicks if bitlink is given (e.g 'bit.ly/2ZHJWq4')", type=str)
     args = parser.parse_args()
-
     link = args.link
 
     bitlink_test = is_bitlink(ACCESS_TOKEN, link)
 
-    if bitlink_test is True:
-        if link.startswith("http://"):
-            newlink = link.replace("http://", "")
-            try:
-                statistics = count_clicks(ACCESS_TOKEN, newlink)
-                print("Количество переходов по ссылке {}: {}".format(link, statistics))
-            except requests.exceptions.HTTPError as error:
-                print("Ошибка: {}".format(error))
-
-        elif link.startswith("https://"):
-            newlink = link.replace("https://", "")
-            try:
-                statistics = count_clicks(ACCESS_TOKEN, newlink)
-                print("Количество переходов по ссылке {}: {}".format(link, statistics))
-            except requests.exceptions.HTTPError as error:
-                print("Ошибка: {}".format(error))
-
-        else:
-            try:
-                statistics = count_clicks(ACCESS_TOKEN, link)
-                print("Количество переходов по ссылке {}: {}".format(link, statistics))
-            except requests.exceptions.HTTPError as error:
-                print("Ошибка: {}".format(error))
-
-    elif link.startswith("http://") or link.startswith("https://"):
+    if bitlink_test:
+        new_link = cut_http(link)
         try:
-            bitlink = shorten_the_link(ACCESS_TOKEN, link)
-            print("Сокращённая ссылка: ", bitlink)
+            statistics = count_clicks(ACCESS_TOKEN, new_link)
+            return "Количество переходов по ссылке {}: {}".format(new_link, statistics)
         except requests.exceptions.HTTPError as error:
-            print("Ошибка: {}".format(error))
+            return "Ошибка: {}".format(error)
 
-    else:
-        print("Ссылка введена неверно")
+    if not bitlink_test:
+        if link.startswith("http://") or link.startswith("https://"):
+            try:
+                bitlink = shorten_the_link(ACCESS_TOKEN, link)
+                return "Сокращённая ссылка: {}".format(bitlink)
+            except requests.exceptions.HTTPError as error:
+                return "Ошибка: {}".format(error)
+
+    return "Ссылка введена неверно"
+
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
